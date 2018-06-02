07e65ab99ff52495e714e38b97a474d67d86547e
Daniel Pettersson <profil@errno.se>
2018-06-02 12:37:38 +0200

    Use custom file

--- init.el
+++ init.el
@@ -215,16 +215,5 @@
 (setq split-height-threshold nil)
 (setq split-width-threshold 160)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(alchemist-test-status-modeline nil)
- '(org-agenda-files (quote ("~/todo.org"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
